476bb2435a7531dcff1e6507018b875ef605a76d
Oezkandemir <oezkan@reign-labs.com>
2026-01-11 20:34:26 +0100

    Fix Android CMake conflict: exclude react-native-worklets via react-native.config.js

---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dependencies: {
+    'react-native-worklets': {
+      platforms: {
+        android: null, // Disable Android autolinking for react-native-worklets
+      },
+    },
+  },
+};
